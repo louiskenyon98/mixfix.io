964fb054e1aedad468f49738f9addf6bdd78232f
Louis Kenyon <louis.kenyon@ageas.co.uk>
2019-09-05 10:34:16 +0100

    initial commit

---
+++ src/app/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+      <h1>Wuddup fam</h1>
+  )
+};
+ReactDOM.render(<App/>, document.getElementById('app'));
