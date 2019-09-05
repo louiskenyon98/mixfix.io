420605d50d436064e94ebb047ec31590f4f33a8a
Louis Kenyon <louis.kenyon@ageas.co.uk>
2019-09-05 10:59:55 +0100

    working webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,83 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const {UnusedFilesWebpackPlugin} = require("unused-files-webpack-plugin");
+
 module.exports = {
+  entry: {
+    main: './src/app/index.jsx'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader',
         }
       },
-      // {
-      //   test: /\.html$/,
-      //   use: [
-      //     {
-      //       loader: "html-loader"
-      //     }
-      //   ]
-      // }
-    ]
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {minimize: true},
+          },
+        ],
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot|png|jpg|gif|svg|base64|mp4)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1000000,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss|css$/,
+        use: [
+          {
+            loader: 'style-loader', // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+            options: {
+              modules: {
+                mode: 'local',
+                localIdentName: '[local]',
+              },
+            }
+          },
+          {
+            loader: 'sass-loader', // compiles Sass to CSS
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
   },
   plugins: [
-    new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html"
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+    new UnusedFilesWebpackPlugin({
+      patterns: ["src/app/**/*.*"]
     })
-  ]
+  ],
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
 };
