2b3728fa2a0174d9593393b1870298d99491509f
Louis Kenyon <louis.kenyon@kenyoncomputing.co.uk>
2019-09-07 20:44:20 +0100

    First working OAUTH with React

--- src/app/components/Login/Login.jsx
+++ src/app/components/Login/Login.jsx
@@ -1,1 +1,19 @@
 import React from 'react';
+import {Button, Fab} from '@material-ui/core'
+
+class Login extends React.Component {
+  render() {
+    return (
+      <Fab
+        href="/login"
+        variant="extended"
+        size="medium"
+        color="primary"
+      >
+        Login
+      </Fab>
+    )
+  }
+}
+
+export default Login
