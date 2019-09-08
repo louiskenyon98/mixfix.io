cb2f7634357cee7acf14558fddb860148bb12202
Louis Kenyon <louis.kenyon@kenyoncomputing.co.uk>
2019-09-08 23:31:03 +0100

    Add content to splash screen

--- src/app/components/Splash/Splash.jsx
+++ src/app/components/Splash/Splash.jsx
@@ -1,1 +1,20 @@
 import React from 'react';
+import {Card, CardContent, CssBaseline, Typography} from '@material-ui/core';
+import Login from '../Login/Login';
+
+const Splash = () => {
+  return (
+    <React.Fragment>
+      <CssBaseline/>
+      <Card>
+        <CardContent>
+          <Typography>
+            Welcome to mixfix.io
+          </Typography>
+          <Login/>
+        </CardContent>
+      </Card>
+    </React.Fragment>
+  )
+};
+export default Splash;
