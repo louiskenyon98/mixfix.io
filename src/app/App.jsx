a32ad7a60d28b14359534bcf53f8fea5262ec3af
Louis Kenyon <louis.kenyon@kenyoncomputing.co.uk>
2019-09-08 23:29:50 +0100

    Add splash screen and success route to app

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,15 +1,17 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Login from "./components/Login/Login";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LoginSuccess from './components/Login/LoginSuccess';
+import Splash from './components/Splash/Splash';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Login/>
+        {/*<Login/>*/}
+        <Splash/>
         <Router>
           <Switch>
-            <Route path={"/bob"} component={() => <div>bob</div>}/>
+            <Route path={"/loginSuccess"} component={LoginSuccess}/>
             <Route path={"/hello"} component={() => <div>hello world</div>}/>
           </Switch>
         </Router>
