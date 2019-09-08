edd743b941ab4903c8e006e336283d456fff81ac
Louis Kenyon <louis.kenyon@kenyoncomputing.co.uk>
2019-09-08 23:31:46 +0100

    Add Spotify icon to Login component and add withStyles

--- src/app/components/Login/Login.jsx
+++ src/app/components/Login/Login.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
-import {Button, Fab} from '@material-ui/core'
+import {Fab, withStyles} from '@material-ui/core'
+import img from '../../../images/spotify-icon.png'
+
+const styles = {
+  root: {
+    background: '#1DB954'
+  },
+  image: {
+    width: '25px',
+    marginRight: '5px'
+  }
+};
 
 class Login extends React.Component {
   render() {
     return (
-      <Fab
-        href="/login"
-        variant="extended"
-        size="medium"
-        color="primary"
-      >
-        Login
-      </Fab>
+      <React.Fragment>
+        <Fab
+          href="/login"
+          variant="extended"
+          size="medium"
+          className={this.props.classes.root}
+        >
+          <img src={img} className={this.props.classes.image} alt={'Spotify Logo'}/>
+          Login with Spotify
+        </Fab>
+      </React.Fragment>
     )
   }
 }
 
-export default Login
+export default withStyles(styles)(Login)
