5b710380bd9593f17566d3c35ae7c8af2d97888b
Louis Kenyon <louis.kenyon@kenyoncomputing.co.uk>
2019-10-08 11:22:43 +0100

    Update auth.js

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,3 +1,4 @@
+//Creds are invalid, remember to change.
 export const client_id = '9f8e38fa4a4b4121a8108c36674f4a67'; // Your client id
 export const redirect_uri = 'http://localhost:8080/callback'; // Your redirect uri
 export const client_secret = 'bc25b96167454fa79682074259f137c2'; // Your secret
