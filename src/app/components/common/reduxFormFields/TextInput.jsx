46af0daf8474a3cd90056e4d2c4f4bbf03f3fb2b
Louis Kenyon <louis.kenyon@kenyoncomputing.co.uk>
2019-10-12 18:01:44 +0100

    MF_FRO-02 - Create TextInput component

---
+++ src/app/components/common/reduxFormFields/TextInput.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {withStyles, TextField} from '@material-ui/core';
+import {theme} from '../../../theme';
+
+const styles = {
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200
+  }
+};
+const TextInput = ({
+  input,
+  label,
+  meta: {touched, error},
+  ...custom
+}) => {
+  return (
+    <TextField
+      label={label}
+      className={this.props.classes.textField}
+      error={touched && error}
+      {...input}
+      {...custom}
+    />
+  )
+};
+export default withStyles(styles)(TextInput);
