8b5dee3549b98ab8db4878d141e237da84e30817
Louis Kenyon <louiskenyon98@gmail.com>
2019-10-15 19:55:29 +0100

    MF_FRO-03 - Add redux-form signup form

---
+++ src/app/containers/SignupFormContainer.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import {Field, reduxForm} from 'redux-form';
+import {Button, Container, CssBaseline} from '@material-ui/core';
+import TextInput from '../components/common/reduxFormFields/TextInput';
+
+
+export class SignupFormContainer extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  onSubmit(formValues) {
+    console.log('signup formValues: ', formValues);
+  }
+
+  render() {
+    return (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline/>
+        <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+          <Field
+            label="Name"
+            name="name"
+            type="text"
+            component={TextInput}
+          />
+          <Field
+            label="Email"
+            name="email"
+            type="email"
+            component={TextInput}
+          />
+          <Field
+            label="Password"
+            name="password"
+            type="password"
+            component={TextInput}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+          >
+            Sign In
+          </Button>
+        </form>
+      </Container>
+    )
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+  const requiredFields = [
+    'name',
+    'email',
+    'password'
+  ];
+  for (let i = 0; i < requiredFields.length; i++) {
+    let field = requiredFields[i];
+    if(!formValues[field]) {
+      errors[field] = 'Required'
+    }
+  }
+  console.log(errors);
+  return errors;
+};
+
+export default reduxForm({
+  form: 'signupForm',
+  validate
+})(SignupFormContainer)
