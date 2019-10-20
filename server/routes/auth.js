2a79f58680aba507cc80a20e2b488c721d479e58
Louis Kenyon <louiskenyon98@gmail.com>
2019-10-20 19:51:18 +0100

    MF_FUL-01 - Add login routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,18 @@
 import express from 'express';
-import {getSignup, postSignup} from "../controllers/auth";
+import {getSignup, postLogin, postSignup} from "../controllers/auth";
+import {determineLoggedInState} from "../util/determineLoggedInState";
 
 const router = express.Router();
 
 router.get('/signup', getSignup);
 router.post('/signup', postSignup);
+
+router.get('/login', (req, res, next) => {
+  console.log(determineLoggedInState(req));
+
+  //Pass off to react-router
+  next();
+});
+router.post('/login', postLogin);
 
 export default router;
