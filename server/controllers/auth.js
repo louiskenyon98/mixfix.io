38310e7f213be360b81b68f256fe455ce30fc111
Louis Kenyon <louiskenyon98@gmail.com>
2019-10-20 18:00:46 +0100

    MF_BAC-02 - Add cookie setting to login route

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,9 +1,24 @@
+import User from "../models/user";
+import {determineLoggedInState} from "../util/determineLoggedInState";
 
+export const postSignup = (req, res, next) => {
 
-export const postSignup = (req, res) => {
-  console.log(req.body);
 };
 
 export const getSignup = (req, res) => {
   res.redirect('/signupform')
+};
+
+export const postLogin = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  req.isLoggedIn = true;
+  console.log(req);
+  res.setHeader('Cache-Control', 'private');
+  res.cookie('isLoggedIn', true);
+  res.send({"redirect": "/"});
+};
+
+export const getLogin = (req, res) => {
+
 };
