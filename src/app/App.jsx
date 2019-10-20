a76884c5cd166ad07659679a98265f406f0da66c
Louis Kenyon <louiskenyon98@gmail.com>
2019-10-20 19:43:29 +0100

    MF_FUL-01 - Add protected route to app

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,9 +4,10 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import SpotifyLoginSuccess from './components/SpotifyLogin/SpotifyLoginSuccess';
 import Splash from './components/Splash/Splash';
-import {theme} from "./theme";
-import SignupFormContainer from "./containers/SignupFormContainer";
-import {PrivateRoute} from "./components/common/PrivateRoute";
+import {theme} from './theme';
+import SignupFormContainer from './containers/SignupFormContainer';
+import {PrivateRoute} from './components/common/PrivateRoute';
+import LoginFormContainer from './containers/LoginFormContainer';
 
 class App extends React.Component {
   render() {
@@ -15,6 +16,7 @@ class App extends React.Component {
         <Router>
           <Switch>
             <Route exact path={"/"} component={Splash}/>
+            <Route exact path={"/login"} component={LoginFormContainer}/>
             <Route path={"/loginSuccess"} component={SpotifyLoginSuccess}/>
             <PrivateRoute path={"/hello"} component={() => <div>This is a private hello world page</div>}/>
             <Route path={"/signup"} component={SignupFormContainer}/>
