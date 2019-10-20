0722c058b063b7f893eb5f4bb441e1e5cac69e93
Louis Kenyon <louiskenyon98@gmail.com>
2019-10-20 19:42:33 +0100

    MF_FUL-01 - Check for cookie to allow access to protected page

--- src/app/components/common/PrivateRoute.jsx
+++ src/app/components/common/PrivateRoute.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Cookie from 'js-cookie';
 import {Route, Redirect} from 'react-router-dom';
 
 export const PrivateRoute = ({component: Component, ...rest}) => {
   return (
     <Route {...rest} render={(props) =>(
-      AUTHENTICATION CHECK HERE === true ? <Component{...props}/> : <Redirect to={'/login'}/>
+      !!Cookie.get('isLoggedIn') === true ? <Component{...props}/> : <Redirect to={'/login'}/>
     )}/>
   )
 };
