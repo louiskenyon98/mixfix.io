3a25e2273dc02d3695f91be6b74509343e6d6455
Louis Kenyon <louiskenyon98@gmail.com>
2019-10-21 18:24:16 +0100

    MF_FUL-01 - User lookup demo functionality added

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -9,17 +9,27 @@ export const getSignup = (req, res) => {
   res.redirect('/signupform')
 };
 
-export const postLogin = (req, res, next) => {
+export const postLogin = async (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  req.isLoggedIn = true;
-  res.setHeader('Cache-Control', 'private');
-  res.cookie('isLoggedIn', true);
-  res.send({"redirect": "/"});
+  // res.setHeader('Cache-Control', 'private');
+  // res.cookie('isLoggedIn', true);
+
+  let user = await User.findById('5dab524e659c863c2c798b24');
+  try {
+    req.session.isLoggedIn = true;
+    req.session.user = user;
+    res.send({"redirect": "/"});
+  } catch (error) {
+    console.log('error!', error);
+  }
 };
 
 export const getLogin = (req, res, next) => {
-  console.log(determineLoggedInState(req));
+  res.setHeader('Cache-Control', 'private');
+  res.cookie('isLoggedIn', false);
+  console.log('session:', req.session.isLoggedIn);
+  console.log('user:', req.session.user);
 
   //Pass off to react-router
   next();
